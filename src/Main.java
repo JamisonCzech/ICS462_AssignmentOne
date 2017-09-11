d86625f13914856e01c62e7d8373d7ee85767b15
Jamison Czech <jamisonczech@gmail.com>
2017-09-10 22:27:55 -0500

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * @author Jamison Czech <A HREF="mailto:main@jamisonczech@gmail.com">
+ * (jamisonczech@gmail.com) </A>
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
